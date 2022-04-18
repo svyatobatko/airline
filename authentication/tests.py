9acca6d7d9a8fe68bebd262b87cc38763072885d
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-04-18 14:36:47 +0300

    Create first test

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+
+class UserModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(username='User', email='user@us.er')
+
+    def test_username_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('username').verbose_name
+        self.assertEquals(field_label, 'username')
+
+    def test_email_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('email').verbose_name
+        self.assertEquals(field_label, 'email')
+
+    def test_username_max_length(self):
+        user = User.objects.get(id=1)
+        max_length = user._meta.get_field('username').max_length
+        self.assertEquals(max_length, 255)
+
